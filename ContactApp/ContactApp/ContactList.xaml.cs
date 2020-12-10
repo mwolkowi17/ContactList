ec755e6743447a27485a037a31ebc3d044a0b239
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-12-10 16:12:40 +0100

    Dodaj pliki projektów.

---
+++ ContactApp/ContactApp/ContactList.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ContactApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContactApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactList : ContentPage
+    {
+        public ContactList()
+        {
+            InitializeComponent();
+            list.ItemsSource = new ObservableCollection<Contact>() {
+                new Contact()
+                {
+                    ContactId=1,
+                    Name= "Marcin Wołkowicz",
+                    AditionalInfo="asdasd"
+
+                },
+                 new Contact()
+                {
+                    ContactId=2,
+                    Name= "Iwona Wołkowicz",
+                    AditionalInfo="afsdasd"
+
+                },
+                  new Contact()
+                {
+                    ContactId=3,
+                    Name= "Łucja Wołkowicz",
+                    AditionalInfo="asdafvfvsd"
+
+                },
+                   new Contact()
+                {
+                    ContactId=4,
+                    Name= "Franek Wołkowicz",
+                    AditionalInfo="avfvfvsdasd"
+
+                }
+            };
+            
+        }
+    }
+}
---
+++ ContactApp/ContactApp/Model/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactApp.Model
+{
+    class Contact
+    {
+        public int ContactId { get; set; }
+        public string Name { get; set; }
+        public string AditionalInfo { get; set; }
+    }
+}
